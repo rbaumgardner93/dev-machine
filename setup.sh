1f2992ce70cd3790de70d604ecc7a755c50f80df
Ryan Baumgardner <r.baumgardner93@gmail.com>
2021-11-12 21:18:48 -0500

    update script to ask for git username and email and some other updates

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -u
+
 # Install Homebrew, if not already installed
 echo "\nTask 1: Installing Homebrew if it does not already exist...\n"
 which brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
@@ -20,34 +22,47 @@ echo "\nTask 4: Installing from Brewfile...\n"
 brew bundle install --file=./brew/Brewfile
 
 # Stow dotfiles
-echo "\nTask 5: stow directories in dotfiles...\n"
+echo "\nTask 5: Stow directories in dotfiles...\n"
 stow brew git kitty nvim tmux zsh
 
+echo "\n Task 6: Set up git name and email...\n"
+echo What name would you like to use for your git config?
+read NAME
+echo What email would you like to use for your git config?
+read EMAIL
+echo -e "[user]\n\tname = $NAME\n\temail = $EMAIL" > ~/.local.gitconfig
+
 # Install .oh-my-zsh
-echo "\nTask 6: Installing Oh My ZSH...\n"
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+echo "\nTask 7: Verify Oh My ZSH exists or install Oh My ZSH...\n"
+OHMYZSH=~/.oh-my-zsh
+if [ -d "$OHMYZSH" ]; then
+    echo "$OHMYZSH exists."
+else
+    echo "$OHMYZSH does not exist. Cloning from github."
+    git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+fi
 
 # Source .zshrc
 source ~/.zshrc
 
 # Install LTS node version
-echo "\nTask 7: Installing LTS version of node...\n"
+echo "\nTask 8: Installing LTS version of node...\n"
 nvm install --lts && nvm use --lts
 
 # Install node packages
-echo "\nTask 8: Installing packages with NPM...\n"
+echo "\nTask 9: Installing packages with NPM...\n"
 npm i -g yarn eslint typescript-language-server typescript
 
 # Install nvim and lua
-echo "\nTask 9: Installing nvim and lua...\n"
+echo "\nTask 10: Installing nvim and lua...\n"
 brew install --HEAD nvim && brew install --HEAD luajit
 
 # Install vim-plug
-echo "\nTask 10: Installing vim-plug...\n"
+echo "\nTask 11: Installing vim-plug...\n"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install vim plugins
-echo "\nTask 11: Installing vim plugins...\n"
+echo "\nTask 12: Installing vim plugins...\n"
 nvim --headless +PlugInstall +qall
 
